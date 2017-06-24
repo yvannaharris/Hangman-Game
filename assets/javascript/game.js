b6d4d08acc48de91e943e6399402ef3dd6c04a48
Yvanna Harris <yvannaharris@yvannas-air.attlocal.net>
2017-06-23 22:10:22 -0400

    Added game reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,9 @@
+window.onload = function() {
+
+// Variables
 var availableLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
 
-var words = [
-	"PUPPY",
-	"FETCH",
-	"BONE",
-	"BATH TIME",
-	"DOG CHOW",
-];
-
-var wins = 0
-
-var word = words[Math.floor(Math.random() * words.length)];
+var word ;
 
 var guess ;
 
@@ -21,106 +14,132 @@ var counter ;
 
 var space;
 
+var score= 0;
 
 
-var answerArray = document.getElementById("current-word");
-correct = document.createElement('ul');
+// Get elements
+var remainguess = document.getElementById("remaining-guesses");
+var wins = document.getElementById("win-record");
 
-for (var i = 0; i < word.length; i++) {
-	correct.setAttribute('id', 'my-word');
-	guess = document.createElement('li');
-	guess.setAttribute('class', 'guess');
+// Guesses ul
+result = function () {
+	var answerArray = document.getElementById("current-word");
+	correct = document.createElement('ul');
+
+		for (var i = 0; i < word.length; i++) {
+			correct.setAttribute('id', 'my-word');
+			guess = document.createElement('li');
+			guess.setAttribute('class', 'guess');
 	
-	if (word[i] === "-") {
-		guess.innerHTML = "-";
-		space = 1;
-	} else {
-		guess.innerHTML = "_";
-	} 
+			if (word[i] === "-") {
+			guess.innerHTML = "-";
+			space = 1;
+			guesses += guess;
+			lives--;
+			} else {
+			guess.innerHTML = "_";
+			} 
 
-	guesses.push(guess);
-	answerArray.appendChild(correct);
-	correct.appendChild(guess);
 
+
+			guesses.push(guess);
+			answerArray.appendChild(correct);
+			correct.appendChild(guess);
+	}
 }
+
 console.log("After loop...");
 
-var remainguess = document.getElementById("remaining-guesses");
-remainguess.innerHTML =lives;
-if (lives < 1) {
-	alert("Back to the Dog House...");
-	answerArray.reset();
-	remainguess.reset();
-}
-for (var i = 0; i < guess.length; i++) {
-	if (counter + space === guesses.length) {
-		alert("Belly Rubs for You!");
+// Show remaining guesses
+comments = function () {
+	remainguess.innerHTML = lives;
+	wins.innerHTML = score;
+	if (lives < 1) {
+		alert("Back to the Dog House...");
+		play();
+		
 	}
+	//for (var i = 0; i < guesses.length; i++) {
+		if (counter === guesses.length && space === 0) {
+			alert("Belly Rubs for You!");
+			score++
+			play();
+		}; 
+	//}
 }
-console.log("After second loop");
+comments();
+	console.log("After second loop");
 
-document.onkeyup =function(event) {
-	var guess = String.fromCharCode(event.keyCode).toUpperCase();
+
+// OnKeyUp Function
+check = function () {
+	document.onkeyup =function(event) {
+		var guess = String.fromCharCode(event.keyCode).toUpperCase();
 	console.log(guess);
-	for (var i = 0; i < word.length; i++) {
-		if (word[i] === guess) {
-			guesses[i].innerHTML = guess;
-			counter += 1
+	//var lettersGuessed = document.getElementById("letters-guessed");
+		lettersGuessed = document.createElement('span');
+		lettersGuessed.setAttribute('id', 'letters-guessed');
+
+		for (var i = 0; i < word.length; i++) {
+			
+			if (word[i] === guess) {
+				guesses[i].innerHTML = guess;
+				counter += 1;
+				document.getElementById("letters-guessed").innerHTML = guess;
+			} else {
+				document.getElementById("letters-guessed").innerHTML = guess;
+			}
+		}
+		var j = (word.indexOf(guess));
+		if (j === -1) {
+			lives -= 1;
+			comments();
+		} else {
+			comments();
 		}
 	}
-	var j = (word.indexOf(guess));
-	if (j === -1) {
-		lives -= 1;
-	}
 }
+check();
+
  console.log("Third loop...");
 
+ //Play
+ play = function () {
+ 	var words = [
+		"PUPPY",
+		"FETCH",
+		"BONE",
+		"BATH TIME",
+		"DOG CHOW",
+		"SCOOBY DOO",
+		"SNOOPY",
+		"DOGGY PADDLE",
+		"LEASH",
+		"COLLAR",
+		"FRISBEE",
+		"LICK",
+		"PET",
+		"BARK",
+		"WAG"
+	];
+
+	word = words[Math.floor(Math.random() * words.length)];
+	console.log(word);
+
+ 	guesses = [ ];
+ 	lives = 10;
+ 	counter = 0;
+ 	space = 0;
+ 	score = 0;
+ 	result();
+ 	comments();
+ }
+
+ play();
 
 
 
 
-// Get a guess 
-//Action/logic that you want to execute when a user presses on a key. 
-//document.onkeyup = function(event) {
-//		var guess = event.key;
-	
-//	if (availableLetters.indexOf(guess) > -1) {
-		//if ((lettersMatched && lettersMatched.indexOf(guess) > -1) 
-		//	|| (lettersGuessed && lettersGuessed.indexOf(guess) > -1)) {
-
-//		}
-	//}
 
 
-
-// Update the array
-	//for (var j = 0; j < word.length; j++) {
-		//if (word[j] === guess) {
-			//answerArray[j] = guess;
-			//remainingLetters--;
-		//}
-	//}
-	
-
-
-
-
-//Record wins 
-//var wins = 0;
-
-//if (remainingLetters == 0) {
-//	wins++;
-//}
-
-//var score = wins
-
-//document.querySelector("#win-record").innerHTML = score;
-
-
-		//for (var j = 0; j < word.length; j++) {
-		
-		//if(word[j] === guess) {
-			//space[i] = guess;
-			//remainingLetters--;
-			//}
-		//}
+}
